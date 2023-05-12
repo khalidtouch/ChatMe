6669a4258864faadbce07eedd06cdaef05cdf701
khalidtouch <khalidhulabaloo9@gmail.com>
2023-05-12 10:45:11 +0100

    added a primary color to statusbar and navigationbar

--- ChatMe/core/designsystem/src/main/java/com/khalidtouch/chatme/core/designsystem/theme/Theme.kt
+++ ChatMe/core/designsystem/src/main/java/com/khalidtouch/chatme/core/designsystem/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.khalidtouch.chatme.core.designsystem.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.annotation.VisibleForTesting
@@ -11,9 +12,13 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 
 @VisibleForTesting
 val LightDefaultColorScheme = lightColorScheme(
@@ -169,6 +174,21 @@ fun CMTheme(
         androidTheme -> TintTheme()
         !disableDynamicTheming && supportsDynamicTheming() -> TintTheme(colorScheme.primary)
         else -> TintTheme()
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window.apply {
+                statusBarColor = colorScheme.primary.toArgb()
+                navigationBarColor = colorScheme.primary.toArgb()
+            }
+
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightNavigationBars = darkTheme
+        }
     }
 
     CompositionLocalProvider(
